7f5eee8d54b04f897e72483e8880a535e716dcd0
Amy Rivera <amyrivera.ar@gmail.com>
2018-02-25 13:58:01 -0500

    App setup

---
+++ app/src/main/java/nyc/c4q/pupapplication/Network/Retrofit.java
@@ -0,0 +1,8 @@
+package nyc.c4q.pupapplication.Network;
+
+/**
+ * Created by AmyRivera on 2/25/18.
+ */
+
+public class Retrofit {
+}
---
+++ app/src/main/java/nyc/c4q/pupapplication/recyclerView/DogViewHolder.java
@@ -0,0 +1,8 @@
+package nyc.c4q.pupapplication.recyclerView;
+
+/**
+ * Created by AmyRivera on 2/25/18.
+ */
+
+public class DogViewHolder {
+}
